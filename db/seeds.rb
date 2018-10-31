90a9229a4b46c90c35ef5062a7026dc70ccc13ab
Frank Alkema <frank.alkema@icloud.com>
2018-10-31 15:10:50 +0100

    Ripoff Yelp is done

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,40 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+restaurants = Restaurant.create(
+  [{
+    name: 'Salsashop',
+    address: 'Amsterdam centraal',
+    category: 'belgian',
+    phone_number: '123123123'
+
+  },
+  {
+    name: '5 guys',
+    address: 'utrecht centraal',
+    category: 'french',
+    phone_number: '123123123'
+  },
+  {
+    name: 'Julias',
+    address: 'Amsterdam centraal',
+    category: 'italian',
+    phone_number: '123123123'
+  },
+  {
+    name: 'Sushi-palace',
+    address: 'Amsterdam centraal',
+    category: 'japanese',
+    phone_number: '123123123'
+  },
+  {
+    name: 'Monsieur Candy',
+    address: 'Rotterdam Centraal',
+    category: 'french',
+    phone_number: '123123123'
+  }]
+  )
